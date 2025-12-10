39f45555a6908a9910c0585f06dec4c36d041572
harshit001-2023 <harshitpande938@gmail.com>
2025-12-10 16:31:16 +0530

    Implemented program for constructor [10/12/2025]

---
+++ Basics/blc/Car.java
@@ -0,0 +1,32 @@
+package Basics.blc;
+
+public class Car {
+    private String model;
+    private int year;
+    private double price;
+
+    public Car(String model, int year, double price) {
+        this.model = model;
+        this.year = year;
+        this.price = price;
+    }
+
+    public void validation(){
+        if (year <= 2010) {
+            System.err.println("Year must be grater than " + year);
+            System.exit(0);
+        } else if (price <= 0) {
+            System.err.println("Price must be positive integer");
+            System.exit(0);
+        } else toString();
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ Basics/elc/ConstructorDemo.java
@@ -0,0 +1,15 @@
+package Basics.elc;
+
+import Basics.blc.Car;
+
+public class ConstructorDemo {
+    static void main() {
+        String model = IO.readln("Enter Car model: ");
+        int year = Integer.parseInt(IO.readln("Enter Car Manufacturing year: "));
+        double price = Double.parseDouble(IO.readln("Enter Car Price: "));
+
+        Car car = new Car(model, year, price);
+        car.validation();
+//        IO.println(car);
+    }
+}
