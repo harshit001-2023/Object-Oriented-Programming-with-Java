1951ffb65001460afd077dd86966989c676981fc
harshit001-2023 <harshitpande938@gmail.com>
2025-12-11 23:31:21 +0530

    Implemented program for setter and getter methods and data hiding [11/12/2025]

--- Basics/elc/RectangleDemo.java
+++ Basics/elc/RectangleDemo.java
@@ -1,4 +1,63 @@
-package Basics.elc;
+/*Assignment 01 :
+----------------
+Create a BLC class Rectangle
+
+Attributes :
+
+names        datatypes
+------       ----------
+width           double-privatre
+height          double-private
+
+Implement a parameterized constructor to initialize the non static Field width and height.
+
+Methods :
+---------
+1)
+Method Name : getArea()
+Parameter : No Parameters
+Return Type : double
+Access modifier : public
+
+In this method returns the area of the rectangle.
+
+2)
+Method Name : getPerimeter()
+Parameter : No Parameters
+Return Type : double
+Access modifier : public
+
+In this method returns the perimeter of the rectangle.
+
+Note : Don't use toString() method
+
+Take one Main class ELC class which is having main method,
+Create a Rectangle object with width 5 and height 10 and call
+the getArea() and getPerimeter() methods on it.
+
+
+Test Cases 1 :
+------------------
+Valid Case: width = 5, height = 10
+
+Sample Input: width = 5, height = 10
+
+Expected Output:
+
+Area: 50.0
+Perimeter: 30.0
+
+Test Cases 2 :
+------------------
+Invalid Case: Negative width
+
+Sample Input: width = -5, height = 10
+
+Expected Output:
+
+Error: Width and height must be non-negative.*/
+
+ Basics.elc;
 
 import Basics.blc.Rectangle;
 
