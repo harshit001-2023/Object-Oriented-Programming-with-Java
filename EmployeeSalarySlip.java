a0122fc896a32bf65385b5e5d1a3683541ad94a5
harshit001-2023 <harshitpande938@gmail.com>
2026-01-05 16:58:13 +0530

    Updated and Solved some Problems [05/01/2026]

---
+++ EmployeeSalarySlip.java
@@ -0,0 +1,66 @@
+class Employee{
+    protected int id;
+    protected String name;
+
+    public Employee(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public double calculateSalary(){
+        return 0.0;
+    }
+}
+
+class FullTimeEmployee extends Employee{
+    protected double salary;
+
+    public FullTimeEmployee(int id, String name,double salary){
+        super(id, name);
+        if(salary <= 0){
+            IO.println("Salary can't be negative!!!");
+            System.exit(0);
+        }
+        this.salary = salary;
+    }
+
+    public double calculateSalary(){
+        return salary;
+    }
+}
+
+class PartTimeEmployee extends Employee{
+    protected double hourlyRate;
+    protected int hoursWorked;
+
+    public PartTimeEmployee(int id, String name, double hourlyRate, int hoursWorked) {
+        super(id, name);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double calculateSalary(){
+        return (hourlyRate*hoursWorked);
+    }
+}
+
+class EmployeeSalarySlip{
+    void main() {
+        int choice = Integer.parseInt(IO.readln());
+        int id = Integer.parseInt(IO.readln());
+        String name = IO.readln();
+
+
+        if (choice == 1) {
+            double salary = Double.parseDouble(IO.readln());
+            FullTimeEmployee fullTimeEmployee = new FullTimeEmployee(id, name, salary);
+
+            IO.println(name + " Salary is : " + fullTimeEmployee.calculateSalary());
+        } else if (choice == 2) {
+            int hourlyRate = Integer.parseInt(IO.readln());
+            int hoursWorked = Integer.parseInt(IO.readln());
+            PartTimeEmployee partTimeEmployee = new PartTimeEmployee(id, name, hourlyRate, hoursWorked);
+            IO.println(name + " Salary is : " + partTimeEmployee.calculateSalary());
+        }
+    }
+}
