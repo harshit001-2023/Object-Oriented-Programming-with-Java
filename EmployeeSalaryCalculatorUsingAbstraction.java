fca76df1a8b26f885467238a90efeaf30adf483d
harshit001-2023 <harshitpande938@gmail.com>
2026-01-09 18:34:59 +0530

    Updated and Solved some MCQS and Problems [09/01/2026]

---
+++ EmployeeSalaryCalculatorUsingAbstraction.java
@@ -0,0 +1,76 @@
+import java.util.*;
+
+class EmpSal {
+    public static void main(String[] args) {
+        // Write your code here
+        var choice = Integer.parseInt(IO.readln("Enter Choice [1/2]: "));
+        String name = IO.readln("Enter your name: ");
+        if (choice == 1) {
+            // Permanent Employee
+            double basicPay = Double.parseDouble(IO.readln("Enter Basic Pay: "));
+            double allowance = Double.parseDouble(IO.readln("Enter Allowance: "));
+
+            PermanentEmployee pE = new PermanentEmployee(name, basicPay, allowance);
+            pE.displayInfo();
+
+        } else if (choice == 2) {
+            // Contract Employee
+            double hourlyRate = Double.parseDouble(IO.readln("Enter hourly rate: "));
+            double hoursWorked = Double.parseDouble(IO.readln("Enter Hours Worked: "));
+
+            ContractEmployee cE = new ContractEmployee(name, hourlyRate, hoursWorked);
+            cE.displayInfo();
+
+        } else {
+            IO.println("Invalid choice!");
+        }
+
+    }
+}
+
+abstract class Employee{
+    protected String name;
+
+    public Employee(String name){
+        this.name = name;
+    }
+
+    abstract double calculateSalary();
+
+    public void displayInfo() {
+        IO.println("Employee Details:");
+        IO.println("Employee Name: " + name);
+        IO.println("Salary: " + calculateSalary());
+    }
+}
+
+class PermanentEmployee extends Employee{
+    protected double basicPay;
+    protected double allowance;
+
+    public PermanentEmployee(String name, double basicPay, double allowance) {
+        super(name);
+        this.basicPay = basicPay;
+        this.allowance = allowance;
+    }
+
+    @Override
+    double calculateSalary() {
+        return basicPay + allowance;
+    }
+}
+class ContractEmployee extends Employee{
+    protected double hourlyRate;
+    protected double hoursWorked;
+
+    public ContractEmployee(String name, double hourlyRate, double hoursWorked) {
+        super(name);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    @Override
+    double calculateSalary() {
+        return hourlyRate * hoursWorked;
+    }
+}
