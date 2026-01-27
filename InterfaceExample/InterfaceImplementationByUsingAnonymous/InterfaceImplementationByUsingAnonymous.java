ab344aaeddbe5a5508e2f3da831961cdc1e6fc46
harshit001-2023 <harshitpande938@gmail.com>
2026-01-27 17:42:16 +0530

    Solved a problem on Factory Method and using Interface [27/01/2026]

--- InterfaceExample/InterfaceImplementationByUsingAnonymous/InterfaceImplementationByUsingAnonymous.java
+++ InterfaceExample/InterfaceImplementationByUsingAnonymous/InterfaceImplementationByUsingAnonymous.java
@@ -1,3 +1,31 @@
+/*Assignment 02 (Day 03)
+-----------------------
+Develop a Program on Factory Method where method return type is interface.
+
+Coding Requirement :
+---------------------
+Take a Functional interface Printer which contains a print() method.
+
+Methods :
+
+1) Method Name     : print()
+   Parameter       : No Parameter
+   Return Type     : void
+   Access Modifier : public
+   Modifier   : abstract
+
+Take another class PrinterDemo.
+
+1) Method Name     : getPrinter()
+   Parameter       : No Parameter
+   Return Type     : Printer [Factory Method (Factory Design Pattern)]
+   Access Modifier : public
+
+   In this method override the print() method of Printer interface, using anonymous inner class WITHOUT REFERENCE and return the anonymous inner class object.
+
+Take an ELC class InterfaceImplementationByUsingAnonymous.
+Create the PrinterDemo object and call the print method.*/
+
 package InterfaceExample.InterfaceImplementationByUsingAnonymous;
 
 interface Printer{
--- InterfaceExample/Payment/PaymentGatewayUsingInterface.java
+++ InterfaceExample/Payment/PaymentGatewayUsingInterface.java
@@ -1,82 +1,13 @@
-/*Assignment 01 :
----------------
-Develop a scenario based program using interface for Payment Processing and apply discount using Factory Method.
-
-Coding Requirements :
-----------------------
-Take an interface called Payment.
-
-Methods :
-
-1) Method Name     : processPayment()
-   Parameter       : No Parameter
-   Return Type     : void
-   Access Modifier : public
-   Modifier	   : abstract
-
-
-2) Method Name     : applyDiscount()
-   Parameter       : No Parameter
-   Return Type     : void
-   Access Modifier : public
-     Modifier	   : abstract
-
-
-Take two implementer classes CreditCardPayment and UPIPayment and override both the abstract methods.
-[See the Test Cases for Output]
-
-
-Take another BLC class PaymentGateway
-
-Methods :
-
-1) Method Name     : initiatePayment()
-   Parameter       : paymentType of type String
-   Return Type     : Payment [Factory Method]
-   Access Modifier : public
-
-In this method, With the help of parameter variable paymentType, receive the string value CreditCardPayment and UPIPayment, return CreditCardPayment object OR UPIPayment object based on the String value passed by the user from the ELC class [Factory Method] with some message like "Initiating Credit Card payment" OR "Initiating UPI payment" [See Test Cases for Details]
-
-
-Take an ELC class called PaymentGatewayUsingInterface with main method. Take IO class and PaymentGateway class object so user can pass either CreditCard OR UPI as a String text and get that type of object from Factory method.
-
-
-Test Cases for Output :
-------------------------
-Test Case 1 :
--------------
-Enter Payment Type [CreditCard / UPI]:
-creditcard
-Initiating Credit Card payment
-Processing Credit Card payment...
-Applying 10% discount for Credit Card payment
-
-Test Case 2:
--------------
-Enter Payment Type [CreditCard / UPI]:
-UPI
-Initiating UPI payment
-Processing UPI payment...
-Applying 5% discount for UPI payment
-
-Test Case 3:
-------------
-Enter Payment Type [CreditCard / UPI]:
-debitcard
-Invalid payment type selected!*/
-
 package InterfaceExample.Payment;
 
-interface Payment{
+interface Payment {
     public abstract void processPayment();
     public abstract void applyDiscount();
 }
 
-class CreditCardPayment implements Payment{
-
+class CreditCardPayment implements Payment {
     @Override
     public void processPayment() {
-        IO.println("Initiating Credit Card payment");
         IO.println("Processing Credit Card payment...");
     }
 
@@ -86,11 +17,9 @@ class CreditCardPayment implements Payment{
     }
 }
 
-class UPIPayment implements Payment{
-
+class UPIPayment implements Payment {
     @Override
     public void processPayment() {
-        IO.println("Initiating UPI payment");
         IO.println("Processing UPI payment...");
     }
 
@@ -100,48 +29,33 @@ class UPIPayment implements Payment{
     }
 }
 
-class PaymentGateway{
-    public Payment initiatePayment(String paymentType){
-        if(paymentType.equalsIgnoreCase("creditcard")){
-            return new CreditCardPayment();
-        }else if(paymentType.equalsIgnoreCase("upipayment")){
-            return new UPIPayment();
+class PaymentGateway {
+    public Payment initiatePayment(String paymentType) {
+        if (paymentType == null) return null;
+
+        switch (paymentType.toLowerCase()) {
+            case "creditcard":
+                IO.println("Initiating Credit Card payment");
+                return new CreditCardPayment();
+            case "upi":
+                IO.println("Initiating UPI payment");
+                return new UPIPayment();
+            default:
+                IO.println("Invalid payment type selected!");
+                return null;
         }
-        return null;
     }
 }
 
-class PaymentGatewayUsingInterface{
-    static void main() {
+class PaymentGatewayUsingInterface {
+    public static void main(String[] args) {
         String paymentType = IO.readln("Enter Payment Type [CreditCard / UPI]: ");
+        PaymentGateway paymentGateway = new PaymentGateway();
+        Payment payment = paymentGateway.initiatePayment(paymentType);
 
-        if (paymentType.equalsIgnoreCase("creditcard")){
-            CreditCardPayment creditCardPayment = new CreditCardPayment();
-            creditCardPayment.processPayment();
-            creditCardPayment.applyDiscount();
-            new PaymentGateway().initiatePayment(paymentType);
-        } else if(paymentType.equalsIgnoreCase("upi")){
-            UPIPayment upiPayment = new UPIPayment();
-            upiPayment.processPayment();
-            upiPayment.applyDiscount();
-            new PaymentGateway().initiatePayment(paymentType);
-        } else{
-            IO.println("Invalid Payment Type selected!");
+        if (payment != null) {
+            payment.processPayment();
+            payment.applyDiscount();
         }
-
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
